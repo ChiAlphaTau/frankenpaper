639b885e0c32fd5ce2f9ddc67dadce27dd994c6d
ChiAlphaTau <70270842+ChiAlphaTau@users.noreply.github.com>
2024-04-19 10:52:25 +0100

    Widened the accepted codes to include multiple pages per document statement.
    More specifically, each document statement is now of the form 'file:pages', where 'pages' is a comma-separated list of single pages ('num') and page ranges ('low-high'). 'pages' may be wrapped in brackets, solely for aesthetic reasons.

--- __main__.py
+++ __main__.py
@@ -5,12 +5,18 @@ else:#So python3 -m frankenpaper works.
     from .lib_frankenpaper import *
 
 #The following stuff makes it so the program works nicely as a command line program, including having a "help" output.
-parser = argparse.ArgumentParser(description="Python program to assemble pages from multiple pdfs into one pdf.\n"+code_help)
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,#To keep my formatting (i.e. leave the whitespace alone).
+    description=
+        "Python program to assemble pages from multiple PDFs into one pdf.\n"
+        +code_help+"\n"
+        +"Any arguments not supplied shall be asked for from the user during the program run."
+    )
 parser.add_argument("--verbose","-v",dest="verbose",help="Whether to output progress or not.",action="store_true")
 parser.add_argument("--dirin","-i",dest="input_dir",help="The directory containing the input files.")
 parser.add_argument("--dirout","-o",dest="output_dir",help="The directory to save the generated pdf to.")
 parser.add_argument("--saveas","-s",dest="output_name",help="The filename (excluding extension) within OUTPUT_DIR to output the pdf to.")
-parser.add_argument("--code","-c",dest="input_code",help="The code for which file to input, in file:num format, semicolon separated.")
+parser.add_argument("--code","-c",dest="input_code",help="The code for which file to input, in file:num format, semicolon separated. If you're supplying this on the command line, then you may need to wrap the code in \"\" quotes.")
 args=parser.parse_args()
 construct_pdf_interactive(input_dir=args.input_dir,
                           output_dir=args.output_dir,
--- lib_frankenpaper.py
+++ lib_frankenpaper.py
@@ -2,29 +2,46 @@ from PyPDF2 import PdfMerger
 import os
 
 code_help="""\
-    Encode each page like 'file:num', separating each by a ';'
-    E.g. Type:
-        alpha:12;beta:1
-    to get page 12 of alpha.pdf, followed by page 1 of beta.pdf.\
+    The input code encodes the desired pages as a ';'-separated list of statements of the form 'file:pages', where:
+        - 'file' is the name of the pdf within the input directory to get the page(s) from, without the .pdf extension
+        - 'pages' consists of a ','-separated list of statements of the form:
+            - 'num', to represent page#num only (1-indexed).
+            - 'low,high' to represent pages low-high inclusive (1-indexed).
+          ('pages' may optionally be wrapped in [],{} and/or () brackets to make it look nice.)
+    E.g.:
+        alpha:12;beta:1-2,5; alpha:{7,18,12-15}
+    would generate page 12 of alpha.pdf, then pages 1, 2, 5 of beta.pdf, then pages 7, 18, 12, 13, 14, 15 of alpha.pdf (in that order).
+\
 """
 
 def construct_pdf(input_dir,output_dir,input_code,output_name,verbose):
     input_files=dict()#To store the opened PDFs
     merger=PdfMerger()#Thing to merge the PDFs.
 
-    for code in input_code.split(";"):#For each page in the requested output.
-        [paper_key,page_num]=code.split(":")
+    for code in input_code.split(";"):#For each document statement in the code.
+        [paper_key,pages_code]=code.split(":")
         
         paper_key=paper_key.strip()
-        if paper_key not in input_files:#If that paper that page is from has not been opened yet, open it.
+        if paper_key not in input_files:#If the paper that page is from has not been opened yet, open it.
             filename = os.path.join(input_dir,f"{paper_key}.pdf")
             input_files[paper_key]=open(filename,"rb")
             if(verbose):    print(f"----Opened {paper_key} as '{filename}'")
 
-        page_num=int(page_num)#Get the page number
+        for page_code in pages_code.strip("[]{}() \t").split(","):#Remove any aesthetic brackets from the list of pages, then split along the commas.
+            page_range=page_code.split("-")
+            if len(page_range)==1:#Then if that entry is of the form 'num', set pages to be only page#num.
+                page_num=int(page_range[0])
+                pages=(page_num-1,page_num)
+                if(verbose):    addition_message = f"Added {paper_key}'s page#{page_num}"
+            elif len(page_range)==2:#Otherwise if that entry is of the form 'low-high', set pages to pages#low-high (inclusive)
+                pages=( int(page_range[0])-1 , int(page_range[1]) )
+                if(verbose):    addition_message = f"Added {paper_key}'s pages#{pages[0]+1}-{pages[1]}"
+            else:#Otherwise page code is invalid; tell the user and skip.
+                print(f"{page_code} is not a valid page code; skipping this entry")
+                continue
         
-        merger.append(fileobj=input_files[paper_key], pages=(page_num-1,page_num))#Add that page to the desired output pdf.
-        if(verbose):    print(f"Added {paper_key}'s page#{page_num}")
+            merger.append(fileobj=input_files[paper_key], pages=pages)#Add the desired pages to the desired output pdf.
+            if(verbose):    print(addition_message)#And output that have done so.
 
     filename=os.path.join(output_dir,f"{output_name}.pdf")
     with open(filename,"wb") as output_file:#Output the desired pdf.
